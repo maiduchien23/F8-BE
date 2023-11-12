67a155b35d7ceb9f9311ba6c1637bac84246df32
maiduchien23 <mdhien2302@gmail.com>
2023-11-12 15:58:31 +0700

    day40

---
+++ F8_BE/f8_javascript/homeword/day40/project A/seeders/20231109031555-user-seeder.js
@@ -0,0 +1,29 @@
+"use strict";
+const bcrypt = require("bcrypt");
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          name: "Hien",
+          email: "mdhien2302@gmail.com",
+          password: bcrypt.hashSync("123456", 10),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
